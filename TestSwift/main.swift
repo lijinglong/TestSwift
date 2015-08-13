8add55d9b583d2dae83182807a47e5722e25b09c
lijinglong <2357607191@qq.com>
2015-08-13 09:55:53 +0800

    Initial Commit

---
+++ TestSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TestSwift
+//
+//  Created by lijinglong on 15/8/13.
+//  Copyright (c) 2015年 lijinglong. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
